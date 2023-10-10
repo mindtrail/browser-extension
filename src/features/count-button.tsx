f0bedf8fd0cd5b28ea31132ce0662920abd0556a
Alex <alexandru.pausan@gmail.com>
2023-10-10 13:28:02 +0300

    Add IconSpinner component and loading state to CountButton

--- src/features/count-button.tsx
+++ src/features/count-button.tsx
@@ -1,10 +1,12 @@
 import { useReducer } from "react"
 
+import { IconSpinner } from "./IconSpinner"
+
 export const CountButton = () => {
-  const [count, increase] = useReducer((c) => c + 1, 0)
+  const [loading, toggleLoading] = useReducer((c) => !c, false)
 
   const handleClick = async () => {
-    increase()
+    toggleLoading()
     console.log(123, document)
     const html = document.documentElement.outerHTML
     const pageTitle = document.title
@@ -28,6 +30,7 @@ export const CountButton = () => {
       greeting: "hello",
       payload
     })
+    toggleLoading()
     // do something with response here, not outside the function
     console.log(response)
   }
@@ -36,13 +39,12 @@ export const CountButton = () => {
     <button
       onClick={handleClick}
       type="button"
+      disabled={loading}
       className="plasmo-flex plasmo-flex-row plasmo-items-center plasmo-px-4 plasmo-py-2 plasmo-text-sm plasmo-rounded-lg plasmo-transition-all plasmo-border-none
       plasmo-shadow-lg hover:plasmo-shadow-md
       active:plasmo-scale-105 plasmo-bg-slate-50 hover:plasmo-bg-slate-100 plasmo-text-slate-800 hover:plasmo-text-slate-900">
-      Count:
-      <span className="plasmo-inline-flex plasmo-items-center plasmo-justify-center plasmo-w-8 plasmo-h-4 plasmo-ml-2 plasmo-text-xs plasmo-font-semibold plasmo-rounded-full">
-        {count}
-      </span>
+      Save
+      {loading && <IconSpinner className="plasmo-ml-2" />}
     </button>
   )
 }
