dbbaabbc4199e07383d68df3d0c65dc4c4a69992
Alex <alexandru.pausan@gmail.com>
2023-12-21 11:34:34 +0200

    Refactor getPageData function to get absolute path for image if it is a relative path

--- src/lib/page-data.ts
+++ src/lib/page-data.ts
@@ -1,11 +1,22 @@
 export const getPageData = (autoSave: boolean = true) => {
   const html = document.documentElement.outerHTML
   const title = document.title
-  const description = // @ts-ignore
-    document.querySelector('meta[name="description"]')?.content
-  // @ts-ignore
-  const image = document.querySelector('meta[property="og:image"]')?.content
   const url = window.location.href
+
+  const description = document
+    .querySelector('meta[name="description"]')
+    ?.getAttribute('content')
+
+  let image = document
+    .querySelector('meta[property="og:image"]')
+    ?.getAttribute('content')
+
+
+  // The image may be a relative path, so we need to make it absolute
+  if (image && !image.startsWith('http')) {
+    const { origin } = window.location
+    image = `${origin}/${image}`
+  }
 
   return {
     url,
