0711d49b5e32478aedb26e48f6a1ab5bc8d6d62f
Alex <alexandru.pausan@gmail.com>
2023-12-23 15:26:04 +0200

    mend

--- src/components/store-button.tsx
+++ src/components/store-button.tsx
@@ -1,21 +1,13 @@
-import cssText from 'data-text:~style.css'
 import Brain from 'react:~/assets/brain.svg'
 
 import { Button } from '~/components/ui/button'
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from '~/components/ui/tooltip'
 
 import { IconSpinner } from './icon-spinner'
-
-export const getStyle = () => {
-  const style = document.createElement('style')
-  style.textContent = cssText
-  return style
-}
 
 interface StoreButtonProps {
   handleClick: () => void
@@ -29,7 +21,7 @@ export const StoreButton = ({ handleClick, loading }: StoreButtonProps) => {
         <Button
           onClick={handleClick}
           disabled={loading}
-          className="relative py-4 px-0 rounded-s-2xl overflow-hidden bg-primary hover:bg-white hover:opacity-100">
+          className="relative py-4 px-0 rounded-s-2xl overflow-hidden bg-white opacity-70 hover:bg-white hover:opacity-100">
           <Brain width={48} height={48} className="fill-red-600" />
           {loading && (
             <span className="absolute flex bg-slate-100/50 w-full h-full justify-center items-center  text-slate-500">
@@ -42,5 +34,3 @@ export const StoreButton = ({ handleClick, loading }: StoreButtonProps) => {
     </Tooltip>
   )
 }
-
-// hsl(221.2 83.2% 53.3%)
