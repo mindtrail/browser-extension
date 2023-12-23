3fd235e84269101e50e4e2b7188d900ee83606b0
Alex <alexandru.pausan@gmail.com>
2023-12-23 15:50:52 +0200

    Updated tw to fixed sizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,49 @@
 /** @type {import('tailwindcss').Config} */
+
+const PX_SIZING = {
+  0: '0px',
+  0.5: '2px',
+  1: '4px',
+  1.5: '6px',
+  2: '8px',
+  2.5: '10px',
+  3: '12px',
+  3.5: '14px',
+  4: '16px',
+  5: '20px',
+  6: '24px',
+  7: '28px',
+  8: '32px',
+  9: '36px',
+  10: '40px',
+  11: '44px',
+  12: '48px',
+  14: '56px',
+  16: '64px',
+  20: '80px',
+  24: '96px',
+  28: '112px',
+  32: '128px',
+  36: '144px',
+  40: '160px',
+  44: '176px',
+  48: '192px',
+  52: '208px',
+  56: '224px',
+  60: '240px',
+  64: '256px',
+  72: '288px',
+  80: '320px',
+  96: '384px',
+}
+
 module.exports = {
   darkMode: ['class'],
   content: ['./**/*.tsx'],
   theme: {
     container: {
       center: true,
-      padding: '2rem',
+      padding: '32px',
       screens: {
         '2xl': '1400px',
       },
@@ -46,6 +84,16 @@ module.exports = {
           foreground: 'hsl(var(--card-foreground))',
         },
       },
+      // Use PX instead of REM
+      padding: PX_SIZING,
+      margin: PX_SIZING,
+      width: PX_SIZING,
+      height: PX_SIZING,
+      maxWidth: PX_SIZING,
+      maxHeight: PX_SIZING,
+      gap: PX_SIZING,
+      flexBasis: PX_SIZING,
+
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
