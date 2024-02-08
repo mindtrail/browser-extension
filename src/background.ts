42d6f4142b62bf3d7025e5057d23049dc468c912
Alex <alexandru.pausan@gmail.com>
2024-02-08 15:20:10 +0200

    Refactor redirect logic in background.ts and fix storage initialization in auto-save.ts

--- src/background.ts
+++ src/background.ts
@@ -175,12 +175,53 @@ async function redirectToAuth() {
     url: `${api.TARGET_HOST}${API.SIGN_IN}?callbackUrl=${API.SUCCESS_LOGIN}`,
   })
 
+<<<<<<< Updated upstream
   // Define the listener inside redirectToAuth to capture loginTab in the closure
   const onTabUpdate = function (tabId: number, changeInfo: any) {
     const url = changeInfo?.url || ''
     // Only some updates inlcude the url, like load, we only listen to those
     if (!url || tabId === loginTab.id) {
       return
+=======
+    // Open the login window
+    chrome.windows.create(
+      {
+        url: `${api.TARGET_HOST}${API.SIGN_IN}?callbackUrl=${API.SUCCESS_LOGIN}`,
+        type: 'popup',
+        width: 800,
+        height: 600,
+        top: 100,
+        left: 200,
+      },
+      (newWindow) => {
+        loginWindow = newWindow
+        loginTabId = newWindow?.tabs[0]?.id
+      },
+    )
+
+    const onTabUpdate = async function (tabId: number, changeInfo: any) {
+      if (tabId !== loginTabId) {
+        return // Ignore updates from tabs not in the login window
+      }
+
+      const url = changeInfo?.url || ''
+      // Only some updates inlcude the url, like load, we only listen to those
+      if (!url) {
+        return
+      }
+
+      const isSuccessLogin =
+        url?.includes(`${API.SUCCESS_LOGIN}`) && !url?.includes(API.SIGN_IN)
+
+      if (isSuccessLogin) {
+        initializeExtension()
+        processMessage(request, sendResponse)
+
+        chrome.tabs.onUpdated.removeListener(onTabUpdate)
+        chrome.windows.onRemoved.removeListener(onWindowClose)
+        chrome.windows.remove(loginWindow.id)
+      }
+>>>>>>> Stashed changes
     }
 
     const isSuccessLogin =
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -62,7 +62,7 @@ export const EXCLUDED_TAGS_FOR_CLIPPING = ['INPUT', 'TEXTAREA', 'BUTTON', 'SELEC
 
 export const HOST = {
   LOCAL: 'http://localhost:3000',
-  REMOTE: 'https://next-postgress-auth-stripe.vercel.app/',
+  REMOTE: 'https://mindtrail-alexpausan.vercel.app/',
 }
 
 export const API = {
