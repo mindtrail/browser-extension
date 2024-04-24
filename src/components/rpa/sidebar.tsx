012e0c292709a504d02c85c3e9093145eca61bef
Alex <alexandru.pausan@gmail.com>
2024-04-24 19:27:57 +0300

    Add mock data and update UI components in RPA sidebar

--- src/components/rpa/sidebar.tsx
+++ src/components/rpa/sidebar.tsx
@@ -2,7 +2,15 @@ import { useCallback, useEffect, useMemo, useReducer, useState } from 'react'
 import type { Dispatch, SetStateAction } from 'react'
 import { Storage } from '@plasmohq/storage'
 import { useStorage } from '@plasmohq/storage/hook'
-import { XIcon, CheckIcon, CircleDotIcon } from 'lucide-react'
+import {
+  XIcon,
+  CheckIcon,
+  CircleDotIcon,
+  MousePointerClickIcon,
+  GlobeIcon,
+  PenLineIcon,
+  TextSelectIcon,
+} from 'lucide-react'
 
 import { getPageData } from '~/lib/page-data'
 import { MESSAGES, STORAGE_KEY } from '~/lib/constants'
@@ -15,6 +23,39 @@ interface SidebarProps {
   settings: SettingsStored
   setSettings: Dispatch<SetStateAction<SettingsStored>>
 }
+
+const mockData = [
+  {
+    type: 'browse',
+    selector: 'url',
+    value: 'https://google.com',
+    icon: GlobeIcon,
+  },
+  {
+    type: 'form-edit',
+    selector: 'input',
+    value: 'Hello, World!',
+    icon: PenLineIcon,
+  },
+  {
+    type: 'click',
+    selector: 'button',
+    value: 'Search',
+    icon: MousePointerClickIcon,
+  },
+  {
+    type: 'click',
+    selector: 'a',
+    value: 'Learn more',
+    icon: MousePointerClickIcon,
+  },
+  {
+    type: 'select',
+    selector: 'p',
+    value: 'first paragraph',
+    icon: TextSelectIcon,
+  },
+]
 
 const SAVED_WEBSITES_CONFIG = {
   key: STORAGE_KEY.SAVED_WEBSITES,
@@ -60,32 +101,60 @@ export const RightSidebar = ({ setSettings }: SidebarProps) => {
         <Typography variant='h5'> Super RPA</Typography>
       </div>
       <div className='w-full flex px-4 py-4'>
-        <Button variant='outline' className='flex gap-2 items-center'>
-          <RecordIcon className='w-4 h-4' />
-          Record workflow
+        <Button variant='outline' size='lg' className='flex gap-4 items-center'>
+          <RecordIcon className='w-5 h-5' />
+          Record Workflow
         </Button>
       </div>
-      <div className='flex flex-1 overflow-auto px-4 py-4'>
-        <div className='flex flex-col gap-8'>
-          <div className='flex items-center gap-4'>
-            <div className='w-8 h-8 rounded-full bg-blue-500 flex items-center justify-center'>
-              <CheckIcon className='h-4 w-4 text-white' />
+      <div className='flex flex-1 overflow-auto py-2 w-full'>
+        <div className='flex flex-col w-full px-2 py-2 cursor-default'>
+          {mockData.map(({ type, value, icon: Icon }, index) => (
+            <div
+              key={index}
+              className={`flex items-center gap-4 px-2 py-4 w-full rounded-lg
+                border border-transparent hover:border-input group/row`}
+            >
+              <div className='flex items-center justify-center w-8 h-8 rounded-full border border-foreground/50'>
+                <Icon className='h-4 w-4 text-foreground/50' />
+                {/* <Typography className='text-primary-foreground'>{index + 1}</Typography> */}
+              </div>
+              <div className='flex flex-col gap-1'>
+                <Typography className='capitalize' variant='small-semi'>
+                  {type}
+                </Typography>
+                <Typography variant='small'>{value}</Typography>
+              </div>
+              <Button
+                variant='ghost'
+                className={`invisible group-hover/row:visible absolute right-4`}
+              >
+                <XIcon className='w-4 h-4' />
+              </Button>
             </div>
-            <div className='text-sm font-medium text-blue-500'>Step 1</div>
-          </div>
-          <div className='flex items-center gap-4'>
-            <div className='w-8 h-8 rounded-full bg-blue-500 flex items-center justify-center'>
-              <Typography className='text-white'>2</Typography>
+          ))}
+
+          <div
+            className={`flex items-center gap-4 px-2 py-2 my-2 w-full
+              rounded-lg border border-dashed`}
+          >
+            <div className='w-8 h-8 rounded-full bg-secondary/70 flex items-center justify-center'>
+              {/* <CheckIcon className='h-4 w-4 text-white' /> */}
+              <Typography className='text-secondary-foreground'>
+                {mockData?.length + 1}
+              </Typography>
             </div>
-            <div className='text-sm font-medium text-blue-500'>Step 2</div>
-          </div>
-          <div className='flex items-center gap-4'>
-            <div className='w-8 h-8 rounded-full bg-gray-500 dark:bg-gray-400 flex items-center justify-center'>
-              <Typography className='text-white'>3</Typography>
+            <div className='flex flex-col gap-1'>
+              <Typography className='capitalize' variant='small-semi'>
+                Next action
+              </Typography>
+              {/* <Typography variant='small'>{item.value}</Typography> */}
             </div>
-            <div className='text-sm font-medium text-gray-500 dark:text-gray-400'>
-              Step 3
-            </div>
+            <Button
+              variant='secondary'
+              className={`invisible group-hover/row:visible absolute right-4`}
+            >
+              <XIcon className='w-4 h-4' />
+            </Button>
           </div>
         </div>
       </div>
