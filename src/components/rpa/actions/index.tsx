02c6c94b5b39c1c8e754b43bec50c6bd7aa370eb
Alin Porumb <alin.porumb@gmail.com>
2024-04-26 12:38:12 +0300

    Rename BuilderItem to Action. Add Actions component

---
+++ src/components/rpa/actions/index.tsx
@@ -0,0 +1,55 @@
+// import { XIcon } from 'lucide-react'
+
+// import { Typography } from '~components/typography'
+// import { Button } from '~/components/ui/button'
+
+import { Action } from './action'
+import { MousePointerClickIcon, GlobeIcon, PenLineIcon } from 'lucide-react'
+
+export function Actions({ events }) {
+  if (!events?.length) return null
+
+  const actions = events.map((event) => {
+    return {
+      type: event.type,
+      selector: event.selector,
+      value: event.value,
+      icon: (() => {
+        switch (event.type) {
+          case 'input':
+            return PenLineIcon
+          case 'click':
+            return MousePointerClickIcon
+          default:
+            return GlobeIcon
+        }
+      })(),
+    }
+  })
+
+  return (
+    <div className='flex flex-col w-full px-2 py-2 cursor-default'>
+      {actions.map(({ type, value, icon: Icon }, index) => (
+        <Action Icon={Icon} type={type} value={value} key={index} />
+      ))}
+
+      {/* <div
+        className={`flex items-center gap-4 px-2 py-4 w-full
+              rounded-lg border border-dashed`}
+      >
+        <div className='w-8 h-8 rounded-full border border-dashed bg-accent/50' />
+        <div className='flex flex-col gap-1'>
+          <Typography className='capitalize text-foreground/50' variant='small-semi'>
+            Next action
+          </Typography>
+        </div>
+        <Button
+          variant='secondary'
+          className={`invisible group-hover/row:visible absolute right-4`}
+        >
+          <XIcon className='w-4 h-4' />
+        </Button>
+      </div> */}
+    </div>
+  )
+}
--- src/components/rpa/recorder/index.tsx
+++ src/components/rpa/recorder/index.tsx
@@ -2,37 +2,38 @@ import React, { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 import listenEvents from './listen-events'
 import { RecordButton } from './record-button'
+import { Actions } from '../actions'
 
 export function FlowRecorder({ flows, setFlows }) {
     const [recording, setRecording] = useState(false)
     const [currentFlowId, setCurrentFlowId] = useState(null)
 
     useEffect(() => {
-        const removeEventListeners = listenEvents(onEvent, recording);
-        return () => removeEventListeners();
-    }, [recording]);
+        const removeEventListeners = listenEvents(onEvent, recording)
+        return () => removeEventListeners()
+    }, [recording])
 
     function onEvent(event) {
         console.log(event)
         if (!recording || !currentFlowId) return
-        setFlows(prevFlows => {
+        setFlows((prevFlows) => {
             const updatedEvents = prevFlows[currentFlowId]
                 ? [...prevFlows[currentFlowId], event]
                 : [event]
             const updatedFlows = { ...prevFlows, [currentFlowId]: updatedEvents }
             localStorage.setItem('flows', JSON.stringify(updatedFlows))
             return updatedFlows
-        });
+        })
     }
 
     function toggleRecording() {
         if (!recording) {
             const newFlowId = uuidv4()
             setCurrentFlowId(newFlowId)
-            window.dispatchEvent(new CustomEvent('reset-last-event-time'));
+            window.dispatchEvent(new CustomEvent('reset-last-event-time'))
         } else {
             setCurrentFlowId(null)
-            window.dispatchEvent(new CustomEvent('reset-last-event-time'));
+            window.dispatchEvent(new CustomEvent('reset-last-event-time'))
         }
         setRecording(!recording)
     }
@@ -40,6 +41,7 @@ export function FlowRecorder({ flows, setFlows }) {
     return (
         <div>
             <RecordButton onClick={toggleRecording} recording={recording} />
+            <Actions events={flows[currentFlowId]} />
         </div>
     )
 }
