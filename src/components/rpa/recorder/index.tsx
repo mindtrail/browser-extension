19f9c890b44db8546b4352f8e7ddf78a1f5b72d6
Alex <alexandru.pausan@gmail.com>
2024-04-30 10:51:44 +0300

    Update event recording logic and flow creation in FlowRecorder component. Add font sizes in Tailwind config

--- src/components/rpa/recorder/index.tsx
+++ src/components/rpa/recorder/index.tsx
@@ -9,46 +9,38 @@ import { createFlow } from '../utils/supabase'
 
 export function FlowRecorder() {
   const [recording, setRecording] = useState(false)
-  const [currentFlow, setCurrentFlow] = useState(null)
+  const [eventsRecorded, setEventsRecorded] = useState([])
 
-  useEffect(() => listenEvents(onEvent, recording), [recording])
+  useEffect(() => listenEvents(recordEvent, recording), [recording])
 
-  function onEvent(event) {
-    console.log(event)
-    if (!recording || !currentFlow) return
-    setCurrentFlow((prevFlow) => {
-      if (!prevFlow) return prevFlow
-      return { ...prevFlow, events: [...prevFlow.events, event] }
-    })
+  function recordEvent(event) {
+    setEventsRecorded((prevEvents) => [...prevEvents, event])
   }
 
   async function toggleRecording() {
-    if (!recording) {
-      setCurrentFlow({
-        name: '',
-        description: '',
-        events: [],
-      })
-    } else {
-      const metadata = await generateFlowName(JSON.stringify(currentFlow.events))
-      await createFlow({
-        name: metadata.name,
-        description: metadata.description,
-        events: currentFlow.events,
-      })
-      setCurrentFlow(null)
-    }
-    window.dispatchEvent(new CustomEvent('reset-last-event-time'))
+    // Async setters. Will update only in the next render, so we can call this here.
     setRecording(!recording)
+    setEventsRecorded([])
+    window.dispatchEvent(new CustomEvent('reset-last-event-time'))
+
+    if (!recording || !eventsRecorded.length) {
+      return
+    }
+
+    const metadata = await generateFlowName(JSON.stringify(eventsRecorded))
+    createFlow({
+      name: metadata?.name,
+      description: metadata?.description,
+      events: eventsRecorded,
+    })
   }
 
-  let events = currentFlow?.events || []
-  events = mergeInputEvents(events)
+  let events = mergeInputEvents(eventsRecorded)
   events = discardClickInputEvents(events)
 
   return (
     <div className='flex flex-col absolute bottom-0 w-full max-h-[75%] border bg-slate-50'>
-      <div className='flex flex-col px-2 py-2'>
+      <div className={`${events?.length ? 'flex' : 'hidden'} flex-col px-2 py-2`}>
         <Actions events={events} debugMode={false} />
       </div>
       <RecordButton onClick={toggleRecording} recording={recording} />
