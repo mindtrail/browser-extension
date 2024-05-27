094eac9932620b4c8a4b2fa067fee0a17c57fa46
Alin Porumb <alin.porumb@gmail.com>
2024-05-27 21:52:11 +0300

    Remove unnecessary delay in urlComponent function

--- src/components/rpa/runner/execution/components/url.ts
+++ src/components/rpa/runner/execution/components/url.ts
@@ -1,9 +1,5 @@
-const delay = 1000
-
-export async function urlComponent({ flowId, event, data, onEventStart, onEventEnd }) {
+export async function urlComponent({ flowId, event, onEventStart, onEventEnd }) {
   await onEventStart(flowId, event)
-  await new Promise((resolve) => setTimeout(resolve, delay))
   await onEventEnd(flowId, event)
   window.location.href = event.url
-  await new Promise((resolve) => setTimeout(resolve, 10 * delay))
 }
